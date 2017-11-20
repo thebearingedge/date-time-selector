46f9d4252e0b633fdefbfe6d7df7b8e24f8ecf07
luke-unifymonitor <luke.gofford@unifymonitor.com>
2017-11-20 16:09:32 +0000

    Updated index exports.

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.DropDownSelect = exports.DashboardDateRange = exports.DateTimeRangeSelector = exports.DateTimeSelector = exports.Calendar = undefined;
+exports.DropDownSelect = exports.DateTimeRangeSelector = exports.DateTimeSelector = exports.Calendar = undefined;
 
 var _Calendar = require('./Calendar');
 
@@ -17,10 +17,6 @@ var _DateTimeRangeSelector = require('./DateTimeRangeSelector');
 
 var _DateTimeRangeSelector2 = _interopRequireDefault(_DateTimeRangeSelector);
 
-var _DashboardDateRange = require('./DashboardDateRange');
-
-var _DashboardDateRange2 = _interopRequireDefault(_DashboardDateRange);
-
 var _DropDownSelect = require('./DropDownSelect');
 
 var _DropDownSelect2 = _interopRequireDefault(_DropDownSelect);
@@ -30,5 +26,4 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 exports.Calendar = _Calendar2.default;
 exports.DateTimeSelector = _DateTimeSelector2.default;
 exports.DateTimeRangeSelector = _DateTimeRangeSelector2.default;
-exports.DashboardDateRange = _DashboardDateRange2.default;
 exports.DropDownSelect = _DropDownSelect2.default;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import Calendar from './Calendar'
 import DateTimeSelector from './DateTimeSelector'
 import DateTimeRangeSelector from './DateTimeRangeSelector'
-import DashboardDateRange from './DashboardDateRange'
 import DropDownSelect from './DropDownSelect'
 
 export {
   Calendar,
   DateTimeSelector,
   DateTimeRangeSelector,
-  DashboardDateRange,
   DropDownSelect
 }
